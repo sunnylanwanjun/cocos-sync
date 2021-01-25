67c8c7a508b1c93313e15c49aad39229b4f97f70
2youyou2 <501251991@qq.com>
2021-01-25 18:50:10 +0800

    fixed sync texture

--- asset/texture.ts
+++ asset/texture.ts
@@ -37,7 +37,7 @@ export interface SyncTextureData extends SyncAssetData {
 export class SyncTexture extends SyncAsset {
     static clsName = 'cc.Texture';
 
-    static calcPath(data: SyncTextureData, sceneData: SyncSceneData) {
+    static calcPath (data: SyncTextureData, sceneData: SyncSceneData) {
         data.srcPath = path.join(sceneData.assetBasePath, data.path);
 
         if (!this.supportFormat(data.srcPath)) {
@@ -54,11 +54,11 @@ export class SyncTexture extends SyncAsset {
         data.dstUrl = `db://assets/${path.join(sceneData.exportBasePath, data.path)}/${subfix}`;
     }
 
-    static supportFormat(path: string) {
+    static supportFormat (path: string) {
         return path.endsWith('.png') || path.endsWith('.tga');
     }
 
-    static async sync(data: SyncTextureData, assetBasePath: string) {
+    static async sync (data: SyncTextureData, assetBasePath: string) {
         if (!this.supportFormat(data.srcPath)) {
             data.detail = await CocosSync.getDetailData(data) as SyncTextureDataDetail;
         }
@@ -113,11 +113,13 @@ export class SyncTexture extends SyncAsset {
             }));
         }
         else {
+            fse.ensureDirSync(path.dirname(data.dstPath));
             fse.copyFileSync(data.srcPath, data.dstPath);
+            await Editor.Message.request('asset-db', 'refresh-asset', data.dstUrl);
         }
     }
 
-    static async load(data: SyncTextureData) {
+    static async load (data: SyncTextureData) {
         if (data.mipmapCount > 1) {
             data.asset = await Promise.all(new Array(data.mipmapCount).fill(0).map(async (mipmapData, index) => {
                 let subfix = `/mipmap_${index}.png`;
